87df068dc8cd21b713173a1d597b3b607711e5cc
LucasIgarashi <lucas20020712@gmail.com>
2024-11-08 11:25:41 -0300

    update: Gerador and Resolvedor

--- dummyagent.py
+++ dummyagent.py
@@ -1,107 +1,94 @@
-# #Dummy account
-
-# async def main():
-#     dummy = DummyAgent("igarashi@magicbroccoli.de", "andorinha123321")
-#     await dummy.start()
-
-# """=========================================================================================="""
-
-# #1º Exemplo: Print
-# import spade
-
-# class DummyAgent(spade.agent.Agent):
-#     async def setup(self):
-#         print("Hello World! I'm agent {}".format(str(self.jid)))
-
-# async def main():
-#     dummy = DummyAgent("igarashi@magicbroccoli.de", "andorinha123321")
-#     await dummy.start()
-
-# if __name__ == "__main__":
-#     spade.run(main())
-
-# """=========================================================================================="""
-
-#2º Exemplo: counter
-import asyncio
 import spade
-from spade import wait_until_finished
 from spade.agent import Agent
 from spade.behaviour import CyclicBehaviour
+from spade.template import Template
+from spade.message import Message
+import random
 
-class DummyAgent(Agent):
-    class MyBehav(CyclicBehaviour):
-        async def on_start(self):
-            print("Starting behaviour . . .")
-            self.counter = 0
+#Agente gerador
+class Gerador(Agent):
+    x = random.randint(-3,3) #NÃO_ESQUECER_DE_VOLTAR_OS_VALORES_ORIGINAIS
+    a=0
+    while a == 0:
+        a = random.randint(-3,3)
+    y = -1 * (a*x)
 
+    class funcao_1grau(CyclicBehaviour):
         async def run(self):
-            print("Counter: {}".format(self.counter))
-            self.counter += 1
-            await asyncio.sleep(1)
+            res = await self.receive(timeout=5)
+            if res:
+                x = float(res.body)
+                x = float( Gerador.a*x + Gerador.y )
+                print("Enviou para " + str(res.sender) + " f(",res.body,")= ",x,"=>",int(x))
+                msg = Message(to=str(res.sender)) 
+                msg.set_metadata("performative", "inform")  
+                msg.body = str(int(x))
+                await self.send(msg)
+
+    class tipo_funcao(CyclicBehaviour):
+        async def run(self):
+            msg = await self.receive(timeout=5)
+            if msg:
+                msg = Message(to=str(msg.sender))
+                msg.set_metadata("performative", "inform")
+                msg.body = "1grau" 
+                await self.send(msg)
+                print("Respondeu para" + str(msg.sender) + " com " + msg.body)
+
 
     async def setup(self):
-        print("Agent starting . . .")
-        b = self.MyBehav()
-        self.add_behaviour(b)
+        t = Template()
+        t.set_metadata("performative","subscribe")
+
+        tf = self.funcao_1grau()
+        print("Funcao de 1o grau: ", Gerador.x)
+        print("Funcao: ", Gerador.a, "x + (", Gerador.y, ")")
+
+        self.add_behaviour(tf,t)
+
+        ft = self.tipo_funcao()
+        template = Template()
+        template.set_metadata("performative", "request")
+        self.add_behaviour(ft, template)
+
+# Agente resolvedor
+class Resolvedor(Agent):
+    class EncontrarX(CyclicBehaviour):
+        async def run(self):
+            x = random.randint(-3,3) 
+            msg = Message(to="gerador@magicbroccoli.de")
+            msg.set_metadata("performative", "subscribe")
+            msg.body = str(x)
+            await self.send(msg)
+            print(f"Enviou x = {x} para o agente Gerador")
+
+            res = await self.receive(timeout=5)
+            if res:
+                resultado = int(res.body)
+                print(f"Recebeu f({x}) = {resultado} do agente Gerador")
+
+                if resultado == 0:
+                    print(f"Encontrou a solução: x = {x}")
+                else:
+                    x = random.randint(-3,3)
+                    print(f"Ajustando x para {x}")
+                    msg.body = str(x)
+                    await self.send(msg)
+
+    async def setup(self):
+        print("Agente Resolvedor iniciado")
+        resolver = self.EncontrarX()
+        template = Template()
+        template.set_metadata("performative", "inform")
+        self.add_behaviour(resolver, template)
 
 async def main():
-    dummy = DummyAgent("igarashi@magicbroccoli.de", "andorinha123321")
-    await dummy.start()
-    print("DummyAgent started. Check its console to see the output.")
 
-    print("Wait until user interrupts with ctrl+C")
-    await wait_until_finished(dummy)
+    gerador = Gerador("gerador@magicbroccoli.de", "Senh@qui12")
+    await gerador.start()
+
+    resolvedor = Resolvedor("igarashi@magicbroccoli.de", "andorinha123321")
+    await resolvedor.start()
 
 if __name__ == "__main__":
     spade.run(main())
-
-# """=========================================================================================="""
-
-#3º Exemplo: kill
-
-# import asyncio
-# import spade
-# from spade.agent import Agent
-# from spade.behaviour import CyclicBehaviour
-
-# class DummyAgent(Agent):
-#     class MyBehav(CyclicBehaviour):
-#         async def on_start(self):
-#             print("Starting behaviour . . .")
-#             self.counter = 0
-
-#         async def run(self):
-#             print("Counter: {}".format(self.counter))
-#             self.counter += 1
-#             if self.counter > 3:
-#                 self.kill(exit_code=10)
-#                 return
-#             await asyncio.sleep(1)
-
-#         async def on_end(self):
-#             print("Behaviour finished with exit code {}.".format(self.exit_code))
-
-#     async def setup(self):
-#         print("Agent starting . . .")
-#         self.my_behav = self.MyBehav()
-#         self.add_behaviour(self.my_behav)
-
-# async def main():
-#     dummy = DummyAgent("igarashi@magicbroccoli.de", "andorinha123321")
-#     await dummy.start()
-
-#     # wait until user interrupts with ctrl+C
-#     while not dummy.my_behav.is_killed():
-#         try:
-#             await asyncio.sleep(1)
-#         except KeyboardInterrupt:
-#             break
-
-#     assert dummy.my_behav.exit_code == 10
-
-#     await dummy.stop()
-
-
-# if __name__ == "__main__":
-#         spade.run(main())
