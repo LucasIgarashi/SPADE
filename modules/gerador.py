3b3b8e331d0bbae30a24d3d1f104b1222cff31fb
LucasIgarashi <lucas20020712@gmail.com>
2024-11-13 23:47:32 -0300

    update: modules updates

--- modules/gerador.py
+++ modules/gerador.py
@@ -1,72 +1,103 @@
 import spade
 from spade.agent import Agent
-from spade.behaviour import CyclicBehaviour
+from spade.behaviour import CyclicBehaviour, OneShotBehaviour
 from spade.template import Template
 from spade.message import Message
 import random
+import time
 
 #Agente gerador
 class Gerador(Agent):
-    # k = random.randint(-100,100)
-    # a = 0
-    # b = 0
-    # c = 0
-    # d = 0
+    grau = 0
+    i = 0
+    a = 0
+    b = 0
+    c = 0
+    d = 0
 
-    # while a == 0:
-    #     a = random.randint(-10,10)
-    # b = random.randint(-10,10)
-    # c = random.randint(-10,10)
-    # d = random.randint(-10,10)
- 
-#Tipificar a função
-    class Tipo_funcao(CyclicBehaviour):
+    while a == 0:
+        a = random.randint(-10,10)
+    b = random.randint(-10,10)
+    c = random.randint(-10,10)
+    d = random.randint(-10,10)
+    
+    #Gera o grau da função
+    class Grau(CyclicBehaviour): #Por quê deve ser CyclicBehaviour e não OneShotBehaviour?
         async def run(self):
             msg = await self.receive(timeout=10)
             if msg:
-                graus = [1, 2, 3]
-                grau = random.choice(graus)
+                print("Gerador: Gerando o grau da função")
+                Gerador.grau = random.randint(2,3)
+                print(f"Gerador: Grau gerado = {Gerador.grau}º")
                 res = Message(to=str(msg.sender))
                 res.set_metadata("performative", "inform")
-                res.body = str(grau)
+                res.body = str(Gerador.grau)
                 await self.send(res)
 
-# #Função
-#     class Funcao(CyclicBehaviour):
-#         async def run(self):
-#             res = await self.receive(timeout=10)
-#             if res:
-#                 x = float(res.body)
-#                 z = int(Gerador.grau)
-#                 if z == 1:
-#                     resultado = float((Gerador.a*(x)) + (Gerador.b))
-#                 elif z == 2:
-#                     resultado = float(Gerador.a*(x**2) + Gerador.b*(x) + Gerador.c)
-#                 else:
-#                     resultado = float(Gerador.a*(x - Gerador.b)*(x - Gerador.c)*(x - Gerador.d))
+    # #Printa a função
+    # class set_Funcao(OneShotBehaviour):
+    #     async def run(self):
+    #         msg = await self.receive(timeout=10)
+    #         if msg:
+    #             if Gerador.grau == 1:
+    #                 print(f"Gerador: f(x) = ({Gerador.a})*x + ({Gerador.b})")
+    #             elif Gerador.grau == 2:
+    #                 print(f"Gerador: f(x) = ({Gerador.a})*x^2 + ({Gerador.b})*x + ({Gerador.c})**2")
+    #             elif Gerador.grau == 3:
+    #                 print(f"Gerador: f(x) = ({Gerador.a})*(x - ({Gerador.b}))*(x - ({Gerador.c}))*(x - ({Gerador.d}))")
+    #             print("===========================================================================")
+    #             res = Message(to=str(msg.sender))
+    #             res.set_metadata("performative", "inform")
+    #             await self.send(res)
 
-#                 print(f"Enviou para o agente Resolvedor f({x}) = {resultado} => {int(resultado)}")
-#                 msg = Message(to=str(res.sender))
-#                 msg.set_metadata("performative", "inform")
-#                 msg.body = str(int(resultado))
-#                 await self.send(msg)
+    #Gera a função
+    class Funcao(CyclicBehaviour):
+        async def run(self):
+            res = await self.receive(timeout=10)
+            if res:
+                x = float(res.body)
+                g = int(Gerador.grau)
+                funcoes = {
+                    1:float((Gerador.a*(x)) + (Gerador.b)),
+                    2:float((Gerador.a*(x**2)) + (2*Gerador.a*(x)) + (Gerador.a**2)),
+                    3:float(Gerador.a*(x - Gerador.b)*(x - Gerador.c)*(x - Gerador.d))
+                    }
+                resultado = funcoes[g]
+                print(f"Gerador: Enviou para o agente Resolvedor f({x}) = {resultado} => {int(resultado)}")
+                msg = Message(to=str(res.sender))
+                msg.set_metadata("performative", "inform")
+                msg.body = str(int(resultado))
+                await self.send(msg)
 
     async def setup(self):
-        #Tipo da função
-        tf = self.Tipo_funcao()
+        #Gera da grau
+        tf = self.Grau()
         t1 = Template()
         t1.set_metadata("performative", "request")
-        self.add_behaviour(tf, t1)
+        self.add_behaviour(tf,t1)
 
-        # #Função
-        # f = self.Funcao()
+        # #Printa a função
+        # sf = self.set_Funcao()
         # t2 = Template()
-        # t2.set_metadata("performative","subscribe")
-        # self.add_behaviour(f,t2)
+        # t2.set_metadata("performative", "subscribe")
+        # self.add_behaviour(sf,t2)
+
+        #Gera função
+        f = self.Funcao()
+        t3 = Template()
+        t3.set_metadata("performative","subscribe")
+        self.add_behaviour(f,t3)
 
 async def main():
     gerador = Gerador("gerador@magicbroccoli.de", "Senh@qui12")
     await gerador.start()
+    while gerador.is_alive():
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            gerador.stop()
+            break
+    print("Gerador: Agente encerrou!")
 
 if __name__=='__main__':
     spade.run(main())
--- modules/resolvedor.py
+++ modules/resolvedor.py
@@ -4,63 +4,75 @@ from spade.behaviour import CyclicBehaviour, OneShotBehaviour
 from spade.template import Template
 from spade.message import Message
 import random
+import time
 
 #Agente resolvedor
 class Resolvedor(Agent):
     grau = 0
-    
-    class Grau(OneShotBehaviour):
-        async def run(self):
-            if Resolvedor.grau == 0:
-                print("================================================")
-                print(f"Agente Resolvedor requisitando o grau da função...")
-                msg = Message(to="gerador@magicbroccoli.de")
-                msg.set_metadata("performative", "request")
-                await self.send(msg)
-                res = await self.receive(timeout=10)
-                if res:
-                    Resolvedor.grau = int(res.body)
-                    print(f"Recebeu do agente Gerador: grau = {Resolvedor.grau}")
-                    print("================================================\n")
 
-    # class Chute(CyclicBehaviour):
+    #Tipifica qual a função
+    class Tipo_funcao(OneShotBehaviour):
+        async def run(self):
+            print("===========================================================================")
+            print("Resolvedor: Requisitando o grau da função")
+            msg = Message(to="gerador@magicbroccoli.de")
+            msg.set_metadata("performative", "request")
+            await self.send(msg)
+            res = await self.receive(timeout=10)
+            if res:
+                Resolvedor.grau = int(res.body)
+                grau = {
+                    1:'linear',
+                    2:'quadrática',
+                    3:'cúbica'
+                    }
+                print(f"Resolvedor: A função é {grau[Resolvedor.grau]}")
+                print("===========================================================================")
+                self.agent.add_behaviour(self.agent.Chute())
+
+    # #Printa a função
+    # class get_Function(OneShotBehaviour):
     #     async def run(self):
-    #         x = random.randint(-100,100) 
     #         msg = Message(to="gerador@magicbroccoli.de")
     #         msg.set_metadata("performative", "subscribe")
-    #         msg.body = str(x)
-    #         print("================================================")
-    #         print(f"Enviou para o agente Gerador x = {x}")
     #         await self.send(msg)
 
-    #         res = await self.receive(timeout=10)
-    #         if res:
-    #             resultado = int(res.body)
-
-    #             if resultado == 0:
-    #                 print(f"Encontrou a solução: x = {x}")
-    #                 print("================================================")
-    #                 await self.agent.stop()
-    #             else:
-    #                 print("Solução diferente de zero, tentando novamento...")
-    #                 print("================================================")
+    #Chuta valores para a raíz da função
+    class Chute(CyclicBehaviour):
+        async def run(self):
+            x = random.randint(-100,100) 
+            msg = Message(to="gerador@magicbroccoli.de")
+            msg.set_metadata("performative", "subscribe")
+            msg.body = str(x)
+            print("===========================================================================")
+            print(f"Resolvedor: Enviou para o agente Gerador x = {x}")
+            await self.send(msg)
+            res = await self.receive(timeout=10)
+            if res:
+                resultado = int(res.body)
+                if resultado == 0:
+                    print(f"Resolvedor: Portanto, a/uma solução é x = {x}")
+                    print("===========================================================================")
+                    await self.agent.stop()
+                else:
+                    print("Resolvedor: Solução diferente de zero, tentando novamento...")
+                    print("===========================================================================")
 
     async def setup(self):
         #Grau
-        g = self.Grau()
-        t1 = Template()
-        t1.set_metadata("performative", "inform")
-        self.add_behaviour(g, t1)
-
-        # #Chute
-        # c = self.Chute()
-        # t2 = Template()
-        # t2.set_metadata("performative", "inform")
-        # self.add_behaviour(c, t2)
+        g = self.Tipo_funcao()
+        self.add_behaviour(g)
 
 async def main():
     resolvedor = Resolvedor("igarashi@magicbroccoli.de", "andorinha123321")
     await resolvedor.start()
+    while resolvedor.is_alive():
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            resolvedor.stop()
+            break
+    print("Resolvedor: Agente encerrou!")
 
 if __name__=='__main__':
     spade.run(main())
