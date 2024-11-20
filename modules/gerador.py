38165d4bf4890384c5799a1852792eaf163321e9
LucasIgarashi <lucas20020712@gmail.com>
2024-11-20 12:24:12 -0300

    update: último update antes de mudar a abordagem do resolvedor

--- modules/gerador.py
+++ modules/gerador.py
@@ -12,18 +12,17 @@ class Gerador(Agent):
 
     class setCoefficients(OneShotBehaviour):
         async def run(self):
-            g = self.agent.grau
             self.agent.coeficientes = [random.randint(-5,5) for _ in range(self.agent.grau + 1)]
             while self.agent.coeficientes[0] == 0:
                 self.agent.coeficientes[0] = random.randint(-5,5)
             for i in range(1, self.agent.grau + 1):
                 self.agent.coeficientes[i] = self.agent.coeficientes[0]*random.randint(-5,5)
-    
+                
     class printFunction(OneShotBehaviour):
         async def run(self):
-            termos = [f"(({coef})*(x^{i}))" for i, coef in enumerate(reversed(self.agent.coeficientes))]
+            termos = list(reversed([f"(({coef})*(x^{i}))" for i, coef in enumerate(reversed(self.agent.coeficientes))]))
             print(termos)
-            funcao = " + ".join(reversed(termos))
+            funcao = " + ".join(termos)
             print(f"Gerador: f(x) = {funcao}")
 
     #Gera o grau da função
@@ -32,7 +31,7 @@ class Gerador(Agent):
             msg_received = await self.receive(timeout=10)
             if msg_received:
                 print("Gerador: Gerando o grau da função")
-                self.agent.grau = random.randint(1,3)
+                self.agent.grau = 1 #random.randint(1,3)
                 self.agent.add_behaviour(self.agent.setCoefficients())
                 self.agent.add_behaviour(self.agent.printFunction())
                 res = Message(to=str(msg_received.sender))
@@ -46,9 +45,9 @@ class Gerador(Agent):
             msg_received = await self.receive(timeout=10)
             if msg_received:
                 x = float(msg_received.body)
-                resultado = sum(coef*(x**i) for i, coef in enumerate(reversed(self.agent.coeficientes)))
-                x_variando = [f"(({coef})*(({x})^{i}))" for i, coef in enumerate(reversed(self.agent.coeficientes))]
-                print(f"Gerador: f({x}) = {' + '.join(reversed(x_variando))}")
+                resultado = (sum(coef*(x**i) for i, coef in enumerate(reversed(self.agent.coeficientes))))
+                x_variando = list(reversed([f"(({coef})*(x^{i}))" for i, coef in enumerate(reversed(self.agent.coeficientes))]))
+                print(f"Gerador: f({x}) = {' + '.join(x_variando)}")
                 print(f"Gerador: Enviou para o agente Resolvedor f({x}) = {resultado} => {int(resultado)}")
                 res = Message(to=str(msg_received.sender))
                 res.set_metadata("performative", "inform")
@@ -56,13 +55,13 @@ class Gerador(Agent):
                 await self.send(res)
 
     async def setup(self):
-        #Gera da grau
+        #Gera grau
         sd = self.setDegree()
         t1 = Template()
         t1.set_metadata("performative", "request")
         self.add_behaviour(sd,t1)
 
-        #Gera função
+        #Resolve a função
         fs = self.FunctionSolver()
         t2 = Template()
         t2.set_metadata("performative","subscribe")
