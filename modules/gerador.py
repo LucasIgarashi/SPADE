d426e9c0cb4b8a7ed70122cb4c730c9598c04a2d
LucasIgarashi <lucas20020712@gmail.com>
2024-11-21 20:54:55 -0300

    update: Gerador - criação da funções somente reais | Resolvedor - resolução inteligente da função

--- modules/gerador.py
+++ modules/gerador.py
@@ -3,65 +3,75 @@ from spade.agent import Agent
 from spade.behaviour import CyclicBehaviour, OneShotBehaviour
 from spade.template import Template
 from spade.message import Message
+from numpy import array, roots, isreal
 import random
 
 #Agente gerador
 class Gerador(Agent):
     grau = 0
     coeficientes = []
-
+    
+    #Gera os coeficientes da função
     class setCoefficients(OneShotBehaviour):
         async def run(self):
-            self.agent.coeficientes = [random.randint(-5,5) for _ in range(self.agent.grau + 1)]
-            while self.agent.coeficientes[0] == 0:
-                self.agent.coeficientes[0] = random.randint(-5,5)
-            for i in range(1, self.agent.grau + 1):
-                self.agent.coeficientes[i] = self.agent.coeficientes[0]*random.randint(-5,5)
-                
-    class printFunction(OneShotBehaviour):
-        async def run(self):
-            termos = list(reversed([f"(({coef})*(x^{i}))" for i, coef in enumerate(reversed(self.agent.coeficientes))]))
-            print(termos)
-            funcao = " + ".join(termos)
-            print(f"Gerador: f(x) = {funcao}")
+            self.agent.coeficientes = self.generate_coefficients(self.agent.grau) 
+        
+        def generate_coefficients(self, grau):
+            coeficientes = [random.randint(-10, 10) for _ in range(grau + 1)]
+            while coeficientes[0] == 0:
+                coeficientes[0] = random.randint(-10, 10)
+            
+            if grau == 1:
+                return self.generate_coefficients(grau)
+
+            if grau == 2:
+                # Verificar discriminante para função quadrática
+                a, b, c = coeficientes
+                discriminante = ((b**2) - (4*a*c))
+                if discriminante < 0:
+                    return self.generate_coefficients(grau)
+
+            if grau == 3:
+                # Verificar se a função cúbica tem todas as raízes reais
+                raizes = roots(array(coeficientes))
+                if not isreal(raizes).all():
+                    return self.generate_coefficients(grau)
+
+            return coeficientes
 
     #Gera o grau da função
     class setDegree(CyclicBehaviour):
         async def run(self):
             msg_received = await self.receive(timeout=10)
             if msg_received:
-                print("Gerador: Gerando o grau da função")
-                self.agent.grau = 1 #random.randint(1,3)
+                print("GERADOR     - Gerando o grau da função")
+                self.agent.grau = random.randint(1, 3)
+                print(f"GERADOR     - Grau = {self.agent.grau}")
                 self.agent.add_behaviour(self.agent.setCoefficients())
-                self.agent.add_behaviour(self.agent.printFunction())
                 res = Message(to=str(msg_received.sender))
                 res.set_metadata("performative", "inform")
                 res.body = str(self.agent.grau)
                 await self.send(res)
 
-    #Resolve a função
+    #Calcula e envia a raiz obtida pelo x dado do Resolvedor
     class FunctionSolver(CyclicBehaviour):
         async def run(self):
             msg_received = await self.receive(timeout=10)
             if msg_received:
                 x = float(msg_received.body)
-                resultado = (sum(coef*(x**i) for i, coef in enumerate(reversed(self.agent.coeficientes))))
-                x_variando = list(reversed([f"(({coef})*(x^{i}))" for i, coef in enumerate(reversed(self.agent.coeficientes))]))
-                print(f"Gerador: f({x}) = {' + '.join(x_variando)}")
-                print(f"Gerador: Enviou para o agente Resolvedor f({x}) = {resultado} => {int(resultado)}")
+                resultado = sum((coef*(x**i)) for i, coef in enumerate(reversed(self.agent.coeficientes)))
+                print(f"GERADOR     - Enviou para o agente Resolvedor f({x}) = {resultado} => {int(resultado)}")
                 res = Message(to=str(msg_received.sender))
                 res.set_metadata("performative", "inform")
                 res.body = str(int(resultado))
                 await self.send(res)
 
     async def setup(self):
-        #Gera grau
         sd = self.setDegree()
         t1 = Template()
         t1.set_metadata("performative", "request")
         self.add_behaviour(sd,t1)
 
-        #Resolve a função
         fs = self.FunctionSolver()
         t2 = Template()
         t2.set_metadata("performative","subscribe")
--- modules/resolvedor.py
+++ modules/resolvedor.py
@@ -1,62 +1,98 @@
 import spade
 from spade.agent import Agent
 from spade.behaviour import CyclicBehaviour, OneShotBehaviour
+from spade.template import Template
 from spade.message import Message
-import random
+from numpy import array, roots, isreal
+from numpy.linalg import solve
 
-#Agente resolvedor
+# Agente resolvedor
 class Resolvedor(Agent):
     grau = 0
 
-    #Tipifica qual a função
+    # Tipifica qual a função
     class setTypeFunction(OneShotBehaviour):
         async def run(self):
             print("===========================================================================")
-            print("Resolvedor: Requisitando o grau da função...")
-            msg_request = Message(to="gerador@magicbroccoli.de")
-            msg_request.set_metadata("performative", "request")
-            await self.send(msg_request)
-            res_received = await self.receive(timeout=10)
-            if res_received:
-                Resolvedor.grau = int(res_received.body)
-                grau = {
-                    1:'linear',
-                    2:'quadrática',
-                    3:'cúbica'
-                    }
-                print(f"Resolvedor: A função é {grau[Resolvedor.grau]}")
+            print("RESOLVEDOR  - Requisitando o grau da função...")
+            msg_request1 = Message(to="gerador@magicbroccoli.de")
+            msg_request1.set_metadata("performative", "request")
+            await self.send(msg_request1)
+            res_received1 = await self.receive(timeout=10)
+            if res_received1:
+                self.agent.grau = int(res_received1.body)
+                graus = {
+                    1: 'linear',
+                    2: 'quadrática',
+                    3: 'cúbica'
+                }
+                print(f"RESOLVEDOR  - Grau = {self.agent.grau}. Logo, a função é {graus[self.agent.grau]}")
                 print("===========================================================================")
-                self.agent.add_behaviour(self.agent.Guess())
+                self.agent.add_behaviour(self.agent.Resolve())
 
-    #Chuta valores para a raíz da função
-    class Guess(CyclicBehaviour):
+    class Resolve(CyclicBehaviour):
         async def run(self):
-            x = random.randint(-50,50)
-            msg_request = Message(to="gerador@magicbroccoli.de")
-            msg_request.set_metadata("performative", "subscribe")
-            msg_request.body = str(x)
-            print("===========================================================================")
-            print(f"Resolvedor: Enviou para o agente Gerador x = {x}")
-            await self.send(msg_request)
-            res_received = await self.receive(timeout=10)
-            if res_received:
-                resultado = int(res_received.body)
-                if resultado == 0:
-                    print(f"Resolvedor: Portanto, a/uma solução é x = {x}")
-                    print("===========================================================================")
-                    await self.agent.stop()
+            resultados = []
+            n_const = [i for i in range(self.agent.grau + 1)]
+
+            for x in n_const:
+                msg_request2 = Message(to="gerador@magicbroccoli.de")
+                msg_request2.set_metadata("performative", "subscribe")
+                msg_request2.body = str(x)
+                print("===========================================================================")
+                print(f"RESOLVEDOR  - Enviou para o agente Gerador x = {x}")
+                await self.send(msg_request2)
+                res_received2 = await self.receive(timeout=10)
+                if res_received2:
+                    resultado = float(res_received2.body)
+                    resultados.append((x, resultado))
+                    print(f"RESOLVEDOR  - Recebeu resultado = {resultado}")
+                    if resultado == 0 and self.agent.grau == 1:
+                        print(f"RESOLVEDOR  - A solução para a função é x = {x}")
+                        await self.agent.stop()
                 else:
-                    print("Resolvedor: Solução diferente de zero, tentando novamento...")
-                    print("===========================================================================")
+                    print(f"RESOLVEDOR  - Não recebeu resposta para x = {x}")
+
+            if len(resultados) == self.agent.grau + 1:
+                A = []
+                B = []
+                for x, fx in resultados:
+                    r = [x**i for i in reversed(range(self.agent.grau + 1))]
+                    A.append(r)
+                    B.append(fx)
+                A = array(A)
+                B = array(B)
+                coeficientes = solve(A, B).round().astype(int) #Pelos valores obtidos com os chutes anteriores, encontra os coeficientes da função
+                f_x = [f"(({coeficientes[i]})*(x^{self.agent.grau - i}))" for i in range(self.agent.grau + 1)]
+                print("===========================================================================")
+                print(f"RESOLVEDOR  - A função gerada é: f(x) = {' + '.join(f_x)}")
+                raizes = roots(coeficientes)
+                raizes_reais = [raiz.real for raiz in raizes if isreal(raiz)]
+                print("===========================================================================")
+
+                # Enviar as raízes reais para o agente Gerador e verificar se f(x) = 0
+                for raiz in raizes_reais:
+                    msg_request3 = Message(to="gerador@magicbroccoli.de")
+                    msg_request3.set_metadata("performative", "subscribe")
+                    msg_request3.body = str(raiz)
+                    await self.send(msg_request3)
+                    res_received3 = await self.receive(timeout=10)
+                    if res_received3:
+                        valor_funcao = float(res_received3.body)
+                        if valor_funcao == 0:
+                            print(f"RESOLVEDOR  - Como f({raiz}) = 0, a raiz {raiz} é solução.")
+                print("===========================================================================")
+                await self.agent.stop()
 
     async def setup(self):
-        #Grau
-        g = self.setTypeFunction()
-        self.add_behaviour(g)
+        stf = self.setTypeFunction()
+        t = Template()
+        t.set_metadata("performative", "inform")
+        self.add_behaviour(stf, t)
 
 async def main():
     resolvedor = Resolvedor("resolvedor@magicbroccoli.de", "Senh@qui12")
     await resolvedor.start()
 
-if __name__=='__main__':
+if __name__ == '__main__':
     spade.run(main())
